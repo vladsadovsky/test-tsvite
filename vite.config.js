115e0d3c192ad021241a07f2a5afbbdb6cf308ae
VladS <vlads_work@outlook.com>
2024-06-27 12:16:19 -0700

    First cut

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import eslint from 'vite-plugin-eslint';
+import { fileURLToPath, URL } from "url";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [eslint()],
+  resolve: {
+    alias: {
+      "./runtimeConfig": "./runtimeConfig.browser",
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+})
